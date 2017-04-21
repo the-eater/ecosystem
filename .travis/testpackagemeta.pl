e0d59900d9bf35df68c0220cb149859661966b6f
eater <hello@eaterofco.de>
2017-04-21 10:47:31 +0200

    Fix test script + don't use prove as it's not verbose

--- .travis/testpackagemeta.pl
+++ .travis/testpackagemeta.pl
@@ -4,9 +4,9 @@ use JSON::Fast;
 use Test;
 use Test::META;
 
-my ($from, $to) = split("...", $*ENV<TRAVIS_COMMIT_RANGE>);
+my ($from, $to) = split("...", %*ENV<TRAVIS_COMMIT_RANGE>);
 
-my $diffproc = run 'git', 'diff', '--no-color', '-p', '-U0', $from, $to, '--', 'META.list', :out;
+my $diffproc = run 'git', 'diff', '--no-color', '-p', '-U0', $to, $from, '--', 'META.list', :out;
 my $metadiff = $diffproc.out.slurp-rest;
 
 if $metadiff ~~ /^\s*$/ {
@@ -45,11 +45,18 @@ say "$amountUrls packages were added";
   for @urls -> $url {
     subtest {
       my $sourcedir;
-      lives-ok {
-        my $meta = from-json($lwp.get($url));
+      my $res = lives-ok {
+        my $resp = $lwp.get($url);
+        if ! defined $resp {
+            fail "$url not reachable";
+            return;
+        }
+
+        my $meta = from-json($resp);
 
         if ! $meta<source-url> {
-          bail-out "no source-url defined in META file";
+            fail "no source-url defined in META file";
+            return;
         }
 
         $_ = $meta<name>;
@@ -58,24 +65,27 @@ say "$amountUrls packages were added";
         my $sourceurl = $meta<source-url>;
         my $git = run "git", "clone", $sourceurl, $sourcedir;
         if $git.exitcode ne 0 {
-          bail-out "Couldn't clone repo " ~ $sourceurl;
+          fail "Couldn't clone repo " ~ $sourceurl;
+          return;
         }
       }, "Downloading $url";
 
-      chdir($sourcedir);
+      if $res {
+          chdir($sourcedir);
 
-      my $*DIST-DIR = $sourcedir.IO;
-      my $*TEST-DIR //= Any;
-      my $*META-FILE //= Any;
-      meta-ok();
+          my $*DIST-DIR = $sourcedir.IO;
+          my $*TEST-DIR //= Any;
+          my $*META-FILE //= Any;
+          meta-ok();
 
-      my $zef = run "zef", "install", "--depsonly", "--/build", ".";
-      ok $zef.exitcode eq 0, "Able to install deps";
-      $zef = run "zef", "test", ".";
-      ok $zef.exitcode eq 0, "Package tests pass";
+          my $zef = run "zef", "install", "--depsonly", "--/build", ".";
+          ok $zef.exitcode eq 0, "Able to install deps";
+          $zef = run "zef", "test", ".";
+          ok $zef.exitcode eq 0, "Package tests pass";
 
-      rm-all($sourcedir.IO);
-      chdir($oldpwd);
+          rm-all($sourcedir.IO);
+          chdir($oldpwd);
+      }
     }, "Checking correctness of $url";
   }
 # When we have a directory first recurse, then remove it
